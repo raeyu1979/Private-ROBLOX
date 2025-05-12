1be446943eb28b05466805c3b14ec9f7c49d3900
q2fs <139041842+q2fs@users.noreply.github.com>
2025-05-12 12:20:20 +0700

    Update UI-Library-Themes.lua

--- Libraries/UI-Library-Themes.lua
+++ Libraries/UI-Library-Themes.lua
@@ -1,128 +1,55 @@
 return function(ReGui)
-    ReGui:DefineTheme("Default", {
-        Text = ReGui.Accent.White,
-        TextDisabled = ReGui.Accent.Gray,
-        ErrorText = ReGui.Accent.Red,
+    ReGui:DefineTheme("Dracula", {
+        TitleAlign = Enum.TextXAlignment.Center,
 
-        FrameBg = ReGui.Accent.Dark,
-        FrameBgActive = ReGui.Accent.Light,
+        Text = Color3.fromRGB(248, 248, 242),
+        TextDisabled = Color3.fromRGB(98, 114, 164),
+        CollapsingHeaderText = Color3.fromRGB(248, 248, 242),
 
-        SliderGrab = ReGui.Accent.Light,
-        ButtonsBg = ReGui.Accent.Light,
-        CollapsingHeaderBg = ReGui.Accent.Light,
-        CollapsingHeaderText = ReGui.Accent.White,
-        CheckMark = ReGui.Accent.Light,
-        ResizeGrab = ReGui.Accent.Light,
-        HeaderBg = ReGui.Accent.Gray,
-        HistogramBar = ReGui.Accent.Yellow,
-        ProgressBar = ReGui.Accent.Yellow,
-        RegionBg = ReGui.Accent.Dark,
-        Separator = ReGui.Accent.Gray,
-        ConsoleLineNumbers = ReGui.Accent.White,
-        MenuBar = ReGui.Accent.ExtraDark,
-        PopupCanvas = ReGui.Accent.Black,
+        FrameBg = Color3.fromRGB(40, 42, 54),
+        FrameBgTransparency = 0.2,
+        FrameBgActive = Color3.fromRGB(68, 71, 90),
+        FrameBgTransparencyActive = 0.1,
 
-        TabText = ReGui.Accent.Gray,
-        TabBg = ReGui.Accent.Dark,
-        TabTextActive = ReGui.Accent.White,
-        TabBgActive = ReGui.Accent.Light,
-        TabsBarBg = Color3.fromRGB(36, 36, 36),
+        CheckMark = Color3.fromRGB(139, 233, 253),
+        SliderGrab = Color3.fromRGB(189, 147, 249),
+        ButtonsBg = Color3.fromRGB(255, 121, 198),
+        CollapsingHeaderBg = Color3.fromRGB(68, 71, 90),
+        RadioButtonHoveredBg = Color3.fromRGB(255, 184, 108),
 
-        ModalWindowDimBg = Color3.fromRGB(230, 230, 230),
-        WindowBg = ReGui.Accent.Black,
-
-        Border = ReGui.Accent.Gray,
-
-        Title = ReGui.Accent.White,
-        TitleBarBg = ReGui.Accent.Black,
-        TitleActive = ReGui.Accent.White,
-        TitleBarBgActive = ReGui.Accent.Dark,
-        TitleBarBgCollapsed = Color3.fromRGB(0, 0, 0),
+        WindowBg = Color3.fromRGB(40, 42, 54),
+        TitleBarBg = Color3.fromRGB(40, 42, 54),
+        TitleBarBgActive = Color3.fromRGB(68, 71, 90),
+        Border = Color3.fromRGB(98, 114, 164),
+        ResizeGrab = Color3.fromRGB(189, 147, 249),
+        RegionBgTransparency = 1
     })
 
-    -- Light Theme
-    ReGui:DefineTheme("Light", {
-        Text = ReGui.Accent.Black,
-        TextDisabled = ReGui.Accent.Gray,
-        ErrorText = ReGui.Accent.Red,
+    ReGui:DefineTheme("Solarized", {
+        TitleAlign = Enum.TextXAlignment.Center,
 
-        FrameBg = ReGui.Accent.White,
-        FrameBgActive = ReGui.Accent.Light,
+        Text = Color3.fromRGB(131, 148, 150),
+        TextDisabled = Color3.fromRGB(93, 107, 116),
+        CollapsingHeaderText = Color3.fromRGB(131, 148, 150),
 
-        SliderGrab = ReGui.Accent.Dark,
-        ButtonsBg = ReGui.Accent.Gray,
-        CollapsingHeaderBg = ReGui.Accent.Gray,
-        CollapsingHeaderText = ReGui.Accent.Black,
-        CheckMark = ReGui.Accent.Gray,
-        ResizeGrab = ReGui.Accent.Gray,
-        HeaderBg = ReGui.Accent.White,
-        HistogramBar = ReGui.Accent.Blue,
-        ProgressBar = ReGui.Accent.Green,
-        RegionBg = ReGui.Accent.LightGray,
-        Separator = ReGui.Accent.Gray,
-        ConsoleLineNumbers = ReGui.Accent.Black,
-        MenuBar = ReGui.Accent.LightGray,
-        PopupCanvas = ReGui.Accent.White,
+        FrameBg = Color3.fromRGB(0, 43, 54),
+        FrameBgTransparency = 0.2,
+        FrameBgActive = Color3.fromRGB(7, 54, 66),
+        FrameBgTransparencyActive = 0.1,
 
-        TabText = ReGui.Accent.Black,
-        TabBg = ReGui.Accent.White,
-        TabTextActive = ReGui.Accent.Black,
-        TabBgActive = ReGui.Accent.Light,
-        TabsBarBg = Color3.fromRGB(245, 245, 245),
+        CheckMark = Color3.fromRGB(88, 110, 117),
+        SliderGrab = Color3.fromRGB(147, 161, 161),
+        ButtonsBg = Color3.fromRGB(238, 232, 213),
+        CollapsingHeaderBg = Color3.fromRGB(7, 54, 66),
+        RadioButtonHoveredBg = Color3.fromRGB(253, 246, 227),
 
-        ModalWindowDimBg = Color3.fromRGB(200, 200, 200),
-        WindowBg = ReGui.Accent.White,
-
-        Border = ReGui.Accent.Gray,
-
-        Title = ReGui.Accent.Black,
-        TitleBarBg = ReGui.Accent.White,
-        TitleActive = ReGui.Accent.Black,
-        TitleBarBgActive = ReGui.Accent.LightGray,
-        TitleBarBgCollapsed = Color3.fromRGB(240, 240, 240),
+        WindowBg = Color3.fromRGB(0, 43, 54),
+        TitleBarBg = Color3.fromRGB(0, 43, 54),
+        TitleBarBgActive = Color3.fromRGB(7, 54, 66),
+        Border = Color3.fromRGB(93, 107, 116),
+        ResizeGrab = Color3.fromRGB(147, 161, 161),
+        RegionBgTransparency = 1
     })
 
-    -- Dark Theme
-    ReGui:DefineTheme("Dark", {
-        Text = ReGui.Accent.White,
-        TextDisabled = ReGui.Accent.Gray,
-        ErrorText = ReGui.Accent.Red,
-
-        FrameBg = ReGui.Accent.Black,
-        FrameBgActive = ReGui.Accent.Dark,
-
-        SliderGrab = ReGui.Accent.Gray,
-        ButtonsBg = ReGui.Accent.Gray,
-        CollapsingHeaderBg = ReGui.Accent.Dark,
-        CollapsingHeaderText = ReGui.Accent.White,
-        CheckMark = ReGui.Accent.Gray,
-        ResizeGrab = ReGui.Accent.Gray,
-        HeaderBg = ReGui.Accent.Black,
-        HistogramBar = ReGui.Accent.Purple,
-        ProgressBar = ReGui.Accent.Cyan,
-        RegionBg = ReGui.Accent.Dark,
-        Separator = ReGui.Accent.Gray,
-        ConsoleLineNumbers = ReGui.Accent.White,
-        MenuBar = ReGui.Accent.ExtraDark,
-        PopupCanvas = ReGui.Accent.Black,
-
-        TabText = ReGui.Accent.Gray,
-        TabBg = ReGui.Accent.Black,
-        TabTextActive = ReGui.Accent.White,
-        TabBgActive = ReGui.Accent.Gray,
-        TabsBarBg = Color3.fromRGB(26, 26, 26),
-
-        ModalWindowDimBg = Color3.fromRGB(100, 100, 100),
-        WindowBg = ReGui.Accent.Black,
-
-        Border = ReGui.Accent.Gray,
-
-        Title = ReGui.Accent.White,
-        TitleBarBg = ReGui.Accent.Black,
-        TitleActive = ReGui.Accent.White,
-        TitleBarBgActive = ReGui.Accent.Dark,
-        TitleBarBgCollapsed = Color3.fromRGB(0, 0, 0),
-    })
-
-    return {"Default", "Light", "Dark"}
+    return {"Dracula", "Solarized"}
 end
